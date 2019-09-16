514d063d156b5469cc9ea038f5aca01da2a9a0e4
”yukininomiya” <y.ninomiya@n-step.co.jp>
2019-09-16 08:09:01 +0000

    ログイン機能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,33 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, succes: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
+  end
 
+  
+  def destroy
+    log_out
+    redirect_to root_url, info:'ログアウトしました'
+  end 
+    
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
   private
   def log_in(user)
     session[:user_id] = user.id
+  end  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
+
+  
+  
+  
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
-end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) 
+  end
+end    
+
